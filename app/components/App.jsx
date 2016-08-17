adc031fb3e02a065dc31ab1165ec76f1cdd8a152
viparthasarathy <viparthasarathy@gmail.com>
2016-08-16 17:07:42 -0700

    Abstract notes, change app to class based component

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,4 +1,32 @@
 import React from 'react';
+import uuid from 'uuid';
 import Notes from './Notes';
 
-export default () => <Notes />;
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry'
+        }
+      ]
+    };
+  }
+  render() {
+    const {notes} = this.state;
+
+    return (
+      <div>
+        <button onClick={() => console.log('add note')}>+</button>
+        <Notes notes={notes} />
+      </div>
+    );
+  }
+}
--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -2,19 +2,10 @@ import React from 'react';
 
 import uuid from 'uuid';
 
-const notes = [
-  {
-    id: uuid.v4(),
-    task: 'Learn React'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Do laundry'
-  }
-];
 
-export default () => (
+
+export default ({notes}) => (
   <ul>{notes.map(note =>
     <li key={note.id}>{note.task}</li>
   )}</ul>
-)
+);
